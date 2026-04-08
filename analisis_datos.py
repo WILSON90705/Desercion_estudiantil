a308d11a8b07163abdc194335dba49f1ce438677
wilsonhacker90 <w.tarrifa90@gmail.com>
2026-04-08 11:09:19 -0500

    cambio realizado en el eda en el analisis de datos

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,5 +1,10 @@
+#modelo Eda
 import pandas as pd
 import mysql.connector
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 
 conexion = mysql.connector.connect(
     host="localhost",
@@ -8,6 +13,33 @@ conexion = mysql.connector.connect(
     database="estudiantes"
 )
 
-df = pd.read_sql("SELECT * FROM V_MODELO_FINAL", con=conexion)
+# Desactivar el modo estricto en esta sesión
+
+cursor = conexion.cursor()
+cursor.execute("SET SESSION sql_mode = sys.list_drop(@@SESSION.sql_mode, 'ONLY_FULL_GROUP_BY')")
+df = pd.read_sql("SELECT * FROM students1", con=conexion)
 df.head()
 print(df.head())
+
+print( )
+print(df.info())
+print( )
+print(df.describe())
+
+print()
+
+print(df.isnull().sum())
+
+print()
+
+print()
+
+#valores perdidos
+miss =  df.isnull().sum()
+miss = miss[miss > 0]
+
+#distribucion de abandonoso
+# Class distribution
+print("Target Distribution:")
+print(df["Dropout"].value_counts())
+print(f"\nDropout rate: {df['Dropout'].mean():.1%}")
